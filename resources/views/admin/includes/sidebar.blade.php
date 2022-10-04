a8905815a0f99b8bc3c0310c5eff0e1357ce8df6
KorobovS <korobovs33@yandex.ru>
2022-10-04 22:31:40 +0300

    index floor

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,25 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Admin Pallet Search</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                 with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-home"></i>
+                    <p>
+                        Этажность склада
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <!-- /.sidebar -->
+</aside>
